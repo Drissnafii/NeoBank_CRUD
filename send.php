cc896fa5301d2f09bc5140013ac39f1f6c1a9e7e
drissnafii <drissnafi3@gmail.com>
2024-12-27 16:09:30 +0100

    implement the logic Of sending data

---
+++ send.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'config.php';
+
+if (isset($_POST["submit"])) {
+    # code...
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
+
+$sql = "INSERT INTO contact (firstname, lastname, email, pass) 
+        VALUES (:firstname, :lastname, :email, :pass)";
+$stmt= $pdo->prepare($sql);
+$stmt->execute([
+    ':firstname'=>$firstname,
+    ':lastname'=>$lastname,
+    ':email'=>$email,
+    ':pass'=>$pass,
+]); 
+}
+header('location: index.php')                               
+?>
