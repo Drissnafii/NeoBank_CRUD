00991feac31bad42084cba7d77ddaf13fd180e92
drissnafii <drissnafi3@gmail.com>
2024-12-27 16:07:50 +0100

    implement the connection logic

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+$host = 'localhost';
+$db = 'drissdb';
+$user = 'root';
+$pass = '';
+
+try {
+  //code...
+  echo '<!DOCTYPE html>';
+  echo '<html lang="en">';
+  echo '<head>';
+  echo '<meta charset="UTF-8">';
+  echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+  echo '<title>Database Connection Status</title>';
+  echo '</head>';
+  echo '<body>';
+
+  echo "<div style=\"background-color: #dcfce7;
+                    padding: 1rem;
+                    text-align: center;
+                    font-weight: bold;
+                    border-radius: 0.25rem;
+                    border: 1px solid #86efac;
+                    color: #166534;
+                   \">You Are Connected to $db !</div>";
+
+  echo '</body>';
+  echo '</html>';
+
+  $dsn = "mysql:host=" .  $host. 
+  ";dbname=" . $db;
+  $pdo = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+  die("conection failded". $e->getMessage());
+}
+
+?>
