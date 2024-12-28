b74477153d6b2d24ad51052b7897a91e44ce5eef
drissnafii <drissnafi3@gmail.com>
2024-12-28 19:56:49 +0100

    understanding and building the logic of CRUD .. (using prrng_r / var_dump to understand the output)

--- read.php
+++ read.php
@@ -2,20 +2,24 @@
     require __DIR__ . '/config/Connection.php';
 
     $stmt = $pdo->prepare("SELECT * from contact");
-    $stmt->execute(); 
-    $contacts = $stmt->fetchAll(); 
+    $stmt->execute();
+    $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
     if ($contacts>0) {
         echo "<table>";
         echo "<tr>
                 <th>id</th><br>   
-                <th>Name</th><br>   
-                <th>Last Name</th>  
-                <th>Email</th> 
+                <th>Name</th><br>
+                <th>Last Name</th>
+                <th>Email</th>
                 <th>Password</th>
             </tr>";
 
             foreach ($contacts as $contact) {
+                echo "<pre>";
+                var_dump($contact); die();
+                echo "</pre>";
                 # code...
                 echo "<tr>
                 <td>$contact[ID]</td>  
