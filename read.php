f8031770b987ed94acef7448e5806f189292fcd8
drissnafii <drissnafi3@gmail.com>
2024-12-29 12:11:27 +0100

    from arr -> obj

--- read.php
+++ read.php
@@ -3,17 +3,19 @@
 
     $stmt = $pdo->prepare("SELECT * from contact");
     $stmt->execute();
-    $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $contacts = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 
     if ($contacts>0) {
-        echo "<table class=w3-table-all w3-centered >";
+        echo "<div class= w3-container>";
+        echo "<table class= w3-table-all w3-centered >";
         echo "<tr>
                 <th>id</th><br>   
                 <th>Name</th><br>
                 <th>Last Name</th>
                 <th>Email</th>
                 <th>Password</th> 
+                <th>Operations</th>
             </tr>";
 
             foreach ($contacts as $contact) {
@@ -24,15 +26,22 @@
 
                 # code...
                 echo "<tr>
-                <td>$contact[ID]</td>  
-                <td>$contact[firstname]</td>  
-                <td>$contact[lastname]</td> 
-                <td>$contact[email]</td>
-                <td>$contact[pass]</td>
+                <td>$contact->ID</td>  
+                <td>$contact->firstname</td>  
+                <td>$contact->lastname</td> 
+                <td>$contact->email</td>
+                <td>$contact->pass</td>
+                <td>
+                    <form method='post'>
+                        <input type='submit' value='edit' name='edite' action=update.php class='w3-button w3-blue' >
+                        <input type='submit' value='delete' name='delete' class='w3-button w3-red' >
+                    </form>
+                </td>
             </tr>";
             }
 
         echo "</table>";
+        echo "</div>";
     } else {
         echo "Pas de ligne pour l'afficher...";
     }
